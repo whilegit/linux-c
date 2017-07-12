b371571e79777b286665371beeaa268b7d27de43
Linzhongren <6215714@qq.com>
2017-07-13 00:15:31 +0800

    继续学习stdio.h里的标准函数

---
+++ cstdio/file.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+/**********************************************
+ * 0. stdio.h概述
+ * 　当程序打开时，有三个流是自动打开的, stdin, stdout和stderr，对应
+ * 　文件描述符的0,1,2
+ * --------------------------------------------------------------------
+ * 1. 打开文件
+ *
+ * FILE *fopen(const char *filename, const char *mode);
+ *
+ * @comment fopen(...)对应open系统调用,如果对要对设备进行明确控制,
+ *　　　　　并不受stdio的输入/输出缓冲区的限制,请使用open系统调用.
+ *          与文件描述符一样，可用的文件流数量也是有限的
+ * @param filename 文件路径
+ * @param mode     读写模式
+ *    "r"或"rb":   以只读方式打开文件
+ *    "w"或"wb":   以只写方式打开文件,并把文件长度截短为零
+ *    "a"或"ab":   以只写方式打开文件,新内容追加到文件尾
+ *    "r+"或"rb+"或"r+b": 以修改方式打开文件(读和写)
+ *    "w+"或"wb+"或"w+b": 以修改方式打开文件,并把文件长度截短为零
+ *    "a+"或"ab+"或"a+b": 以修改方式打开文件,新内容追加在文件尾
+ *    字母b表示二进制文件,在Linux中不区分文件是文本文件还是二进制文件,
+ *　　　　所以此处b并无特别作用.
+ * @return 成功: 关联一个文件流,并返回一个FILE结构体指针
+ *         失败: 返回NULL
+ *
+ *---------------------------------------------------------------------------
+ * 2. 读取文件
+ *  
+ * size_t fread(void *ptr, size_t size, size_t nitems, File *stream);
+ *
+ * @param ptr    读取的缓冲区,任意类型
+ * @param size   每个数据记录的长度
+ * @param ntimes 数据记录计数器
+ * @param stream FILE流
+ * @return 返回的记录个数(如果文件提前结束，该值可能小于nitems.) 
+ * @comment  实际从文件读取的字节数是size * nitems，也可能比这个要小一些
+ *
+ * -------------------------------------------------------------------------
+ * 3. 写数据到文件
+ *
+ * size_t fwrite(const void *ptr, size_t size, size_t nitems, FILE *stream);
+ *
+ * @param ptr    准备写入文件的缓冲区
+ * @param size   每个数据记录的长度
+ * @param nitems 数据计录的个数
+ * @param stream FILE流 
+ * @return 返回成功写入的记录个数
+ * 
+ * @备注: 如果是ptr中存放结构化数据，持久化到文件时可能不具备移植性
+ * 　　　　原因是int的大小端问题和struct空隙等
+ * @备注: 在Linux里，文本文件的末尾都应该添加一个\n换行符。
+ * 　　　 所以每次vim编辑结束后，都自动地在文末添加一个\n换行符。
+ *        发现这个现象的原因是vim后的文件字节数比预期的要大几个字节。
+ *        fwrite不会自动添加\n换行符，文件的字节数符合预期。
+ * ------------------------------------------------------------------------
+ * 4. 关闭文件
+ *
+ * int fclose(FILE * stream);
+ *
+ * @param stream FILE流
+ * @return 成功0 失败-1
+ * 
+ *********************************************/
+void fopen_test(){
+   FILE* f = fopen("file.tmp","r"); 
+   char buf[1024];
+   memset(buf, '*', 32);
+   int len = fread(buf, 1, 10, f);
+   buf[10] = 0x00;
+   printf("%d bytes read\n", len);
+   printf("%s\n", buf);
+   fclose(f);
+
+   f = fopen("file.tmp", "a+");
+   fwrite("ABCDEFG\n",1,8,f);
+
+   fclose(f);   
+}
+int main(){
+    fopen_test();
+    return 0;
+}
